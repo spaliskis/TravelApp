d79d87149ed9d9c979a0c14d544feca0a3550c0f
Sarunas Paliskis <sar.paliskis@gmail.com>
2021-12-05 16:27:58 +0200

    Limiting amount of places by distance, only showing places that are at least 10 km away from departure and arrival, sorting markers by distance from departure

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -32,27 +32,49 @@ export default function MapScreen({ navigation }: Props) {
     const [markers, setMarkers] = useState<Array<object>>();
 
     const createRoute = async () => {
+        // Fetching google route from departure point to arrival point
         let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${departure}&destination=${arrival}&key=${GOOGLE_MAPS_API_KEY}`);;
         let respJson = await resp.json();
+        let dirAPIdistance = 0;
+        respJson.routes[0].legs.forEach((leg: object) => {
+            dirAPIdistance += leg.distance.value;
+        });
+        const placesLimit = Math.ceil(dirAPIdistance / 30000);
+        console.log(placesLimit);
         let points = PLdecoder.decode(respJson.routes[0].overview_polyline.points);
         let coordinates = points.map((point: any[]) => {
             return {
                 latitude: point[0],
                 longitude: point[1]
             }
-        })
+        });
+        // Converting coordinates to format suitable for TomTom API
         let tomtomCoords = coordinates.map((point) => {
             return {
                 lat: point.latitude,
                 lon: point.longitude
             }
-        })
+        });
+        // These two loops remove coordinates that are 10km or closer from both departure and arrival points
+        for (let i = 0; i < tomtomCoords.length; i += 10) {
+            if (distance(points[0][0], points[0][1], tomtomCoords[i].lat, tomtomCoords[i].lon) > 10) {
+                tomtomCoords.splice(0, i);
+                break;
+            }
+        }
+        for (let i = tomtomCoords.length-1; i >= 0; i -= 10) {
+            if (distance(points[points.length-1][0], points[points.length-1][1], tomtomCoords[i].lat, tomtomCoords[i].lon) > 10) {
+                tomtomCoords.splice(i, tomtomCoords.length);
+                break;
+            }
+        }
+        // Creating body for TomTom along search API, getting places from the API
         let placesReqBody = {
             route: {
                 points: tomtomCoords,
             }
         }
-        let placesRes = await fetch(`https://api.tomtom.com/search/2/searchAlongRoute/tourist attraction.json?maxDetourTime=1000&limit=5&spreadingMode=auto&key=${TOMTOM_API_KEY}`, {
+        let placesRes = await fetch(`https://api.tomtom.com/search/2/searchAlongRoute/tourist attraction.json?maxDetourTime=1200&limit=${placesLimit}&spreadingMode=auto&key=${TOMTOM_API_KEY}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -60,26 +82,32 @@ export default function MapScreen({ navigation }: Props) {
             body: JSON.stringify(placesReqBody)
         });
         let placesJson = await placesRes.json();
-        let filteredPlaces = placesJson.results.sort((res1, res2) => res1.score < res2.score).slice(0, 5);
+        // let filteredPlaces = placesJson.results.sort((res1, res2) => res1.score < res2.score).slice(0, 5);
+        // Assigning markers from the returned places and sorting them by distance
         let locationMarkers = [];
-        for (let i = 0; i < filteredPlaces.length; i++) {
+        for (let i = 0; i < placesJson.results.length; i++) {
+            const distFromDep = distance(points[0][0], points[0][1], placesJson.results[i].position.lat, placesJson.results[i].position.lon);
             const marker = {
-                latitude: filteredPlaces[i].position.lat,
-                longitude: filteredPlaces[i].position.lon,
-                description: filteredPlaces[i].poi.name,
+                latitude: placesJson.results[i].position.lat,
+                longitude: placesJson.results[i].position.lon,
+                description: placesJson.results[i].poi.name,
+                distFromDep
             }
             locationMarkers.push(marker);
         }
+        locationMarkers = locationMarkers.sort((marker1, marker2) => marker1.distFromDep - marker2.distFromDep);
+        // formating markers locations to be inserted into TomTom calculateRoute URL
         let waypointsArr = locationMarkers.map(marker => {
             return `${marker.latitude}%2C${marker.longitude}`
-        })
+        });
         waypointsArr.unshift(`${points[0][0]}%2C${points[0][1]}`);
         waypointsArr.push(`${points[points.length - 1][0]}%2C${points[points.length - 1][1]}`);
         let waypointsURL = waypointsArr.join('%3A');
+        // Calculating the route with places to visit and assigning it's coordinates to the coords state
         let waypRes = await fetch(`https://api.tomtom.com/routing/1/calculateRoute/${waypointsURL}/json?computeBestOrder=false&avoid=unpavedRoads&key=${TOMTOM_API_KEY}`);
         let waypResJson = await waypRes.json();
         console.log(
-            `\nDistance: ${waypResJson.routes[0].summary.lengthInMeters} m\nTime: ${waypResJson.routes[0].summary.travelTimeInSeconds / 60} min\nDeparture time: ${waypResJson.routes[0].summary.departureTime}\nArrival time: ${waypResJson.routes[0].summary.arrivalTime}\n`)
+            `\nDistance: ${waypResJson.routes[0].summary.lengthInMeters/1000} km\nTime: ${waypResJson.routes[0].summary.travelTimeInSeconds / 60} min (only driving)\nDeparture time: ${waypResJson.routes[0].summary.departureTime}\nArrival time: ${waypResJson.routes[0].summary.arrivalTime}\n`)
         let waypCoords = [];
         waypResJson.routes[0].legs.forEach(leg => {
             for (let point of leg.points) {
@@ -88,7 +116,7 @@ export default function MapScreen({ navigation }: Props) {
                     longitude: point.longitude
                 })
             }
-        })
+        });
         setMarkers(locationMarkers);
         setCoords(waypCoords);
     };
@@ -121,6 +149,28 @@ export default function MapScreen({ navigation }: Props) {
         let reversebound = lowerBound.reverse();
         let fullPoly = upperBound.concat(reversebound);
         return fullPoly;
+    }
+
+    const distance = (depLat: number, depLon: number, markerLat: number, markerLon: number) => {
+        depLat = depLat * Math.PI / 180;
+        depLon = depLon * Math.PI / 180;
+        markerLat = markerLat * Math.PI / 180;
+        markerLon = markerLon * Math.PI / 180;
+        // Haversine formula
+        let dlon = markerLon - depLon;
+        let dlat = markerLat - depLat;
+        let a = Math.pow(Math.sin(dlat / 2), 2)
+            + Math.cos(depLat) * Math.cos(markerLat)
+            * Math.pow(Math.sin(dlon / 2), 2);
+
+        let c = 2 * Math.asin(Math.sqrt(a));
+
+        // Radius of earth in kilometers. Use 3956
+        // for miles
+        let r = 6371;
+
+        // calculate the result
+        return (c * r);
     }
 
     return (
